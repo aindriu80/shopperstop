e6a1ac5750b22a65a47349044598a7cf558c0b16
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-12-03 20:55:11 +0000

    Creating the UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Grocery from "./components/Grocery";
+import ShoppingBag from "./components/ShoppingBag";
+import Stats from "./components/Stats";
 
 class App extends Component {
   render() {
-    return <div className="App">Shoppers Stop</div>;
+    return (
+      <div>
+        <Grocery />
+        <ShoppingBag />
+        <Stats />
+      </div>
+    );
   }
 }
 
